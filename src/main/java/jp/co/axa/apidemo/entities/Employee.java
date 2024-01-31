9db7d735fc3ecb89f1bf8586c7c16d780040c08c
EmanueleDeFonte <defonte.emanuele@gmail.com>
2024-01-31 22:53:49 +0900

    Emanuele - Added username and password (for auth purpose), email and update of department/salary. Added also validation rules.

--- src/main/java/jp/co/axa/apidemo/entities/Employee.java
+++ src/main/java/jp/co/axa/apidemo/entities/Employee.java
@@ -1,38 +1,57 @@
 package jp.co.axa.apidemo.entities;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.math.BigDecimal;
 
 @Entity
-@Table(name="EMPLOYEE")
+@Table(name = "EMPLOYEE")
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 
-    @Getter
-    @Setter
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
     private Long id;
 
-    @Getter
     @Setter
-    @Column(name="EMPLOYEE_NAME")
+    @NotBlank(message = "Employee name cannot be empty.")
+    @Column(name = "EMPLOYEE_NAME", nullable = false)
     private String name;
 
-    @Getter
     @Setter
-    @Column(name="EMPLOYEE_SALARY")
-    private Integer salary;
+    @Min(value = 1, message = "Salary must be greater than 0")
+    @Column(name = "EMPLOYEE_SALARY", nullable = false)
+    private BigDecimal salary;
 
-    @Getter
     @Setter
-    @Column(name="DEPARTMENT")
-    private String department;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "DEPARTMENT", nullable = false)
+    private DepartmentEnum department;
+
+    @Setter
+    @Size(min = 3, max = 50, message = "Username length must be between 3 and 50 characters.")
+    @Column(name = "EMPLOYEE_USERNAME", nullable = false)
+    private String employeeUsername;
+
+    @Setter
+    @Email
+    @Column(name = "EMPLOYEE_EMAIL", nullable = false)
+    private String email;
+
+    @Setter
+    @Size(min = 6, max = 72, message = "Password length must be between 6 and 72 characters.")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$", message = "Password must contain at least one digit, one lowercase and one uppercase character.")
+    @Column(name = "EMPLOYEE_PASSWORD", nullable = false)
+    @JsonIgnore
+    private String employeePassword;
+
 
 }
