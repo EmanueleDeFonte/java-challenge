d83e9007a1e2c274ac9aba57fd8a6a41cbd05517
EmanueleDeFonte <defonte.emanuele@gmail.com>
2024-01-31 22:54:23 +0900

    Emanuele - DB Data Initialization during application bootstrapping

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO EMPLOYEE(`EMPLOYEE_NAME`, `EMPLOYEE_SALARY`, `DEPARTMENT`, `EMPLOYEE_EMAIL`, `EMPLOYEE_USERNAME`, `EMPLOYEE_PASSWORD`)
+VALUES
+  ('Sophia Brown', 1823.50, 'DEPARTMENT_5', 'sophia.brown@email.com', 'SophiaB', '$2b$12$DNbgepfX4nnYD2F99we68.WAF3gRiDefkhWsGWsLGeU29KPi/8EVa'),
+  ('James Smith', 1924.21, 'DEPARTMENT_1', 'james.smith@email.com', 'JamesS', '$2b$12$1BXGOT6cWS7CQkfQk4SvuuEhyQ0B6pa5HzTBvUo71nOTbKKYE8OOK'),
+  ('Isabella Johnson', 2035.33, 'DEPARTMENT_3', 'isabella.johnson@email.com', 'IsabellaJ', '$2b$12$vDHwp3wQSKXK86lsJ2N2HO3cY4iKNo4odjAelWKKxlJuGZcTHXcAq'),
+  ('Lucas White', 2146.27, 'DEPARTMENT_2', 'lucas.white@email.com', 'LucasW', '$2b$12$Wf5a5Llgb4KPYrQRxMDTY.MmolW/NMiOmyuRlmBkhtf.et.tWiyb.'),
+  ('Emma Low', 2257.99, 'DEPARTMENT_4', 'emma.low@email.com', 'EmmaL', '$2b$12$52iqE0JqHlrN/xZh7TuBTegr5st2csCJjE6JYWeSGXL3xB3MWP9Pi');
