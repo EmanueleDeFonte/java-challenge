13584f7ce02a9c93a6e42ac6960070951fbb091d
EmanueleDeFonte <defonte.emanuele@gmail.com>
2024-02-04 19:43:32 +0900

    Emanuele - REST API to shut down application

--- src/main/java/jp/co/axa/apidemo/ApiDemoApplication.java
+++ src/main/java/jp/co/axa/apidemo/ApiDemoApplication.java
@@ -3,17 +3,35 @@ package jp.co.axa.apidemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
 @EnableCaching
 @EnableRedisRepositories
+@RestController
 public class ApiDemoApplication {
 
+	private static ConfigurableApplicationContext context;
+
 	public static void main(String[] args) {
-		SpringApplication.run(ApiDemoApplication.class, args);
+
+		context = SpringApplication.run(ApiDemoApplication.class, args);
+
+	}
+
+	@PostMapping("/shutdown")
+	public void shutdown() {
+		if (context != null) {
+
+			SpringApplication.exit(context);
+
+		}
+
 	}
 
 }
